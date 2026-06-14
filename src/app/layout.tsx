d61bf2f7764d9b653483fb4f45e960117da60a29
EvilDron09 <andrej32118@gmail.com>
2026-06-14 22:19:57 +0300

    Links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {Menu} from "@/components/menu/Menu";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +30,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-      root app layout
+        <Menu/>
       {children}
+      <hr/>
       </body>
     </html>
   );
---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export const Menu = () => {
+    return (
+        <>
+            <ul>
+                <li>
+                    <Link href="/public">Home</Link>
+                </li>
+                <li>
+                    <Link href="about">About</Link>
+                </li>
+                <li>
+                    <Link href="contacts">Contacts</Link>
+                </li>
+                <li>
+                    <Link href="users">Users</Link>
+                </li>
+            </ul>
+            <hr/>
+        </>
+    );
+};

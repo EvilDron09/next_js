0b5f89afd0ad4db3fe18bea042fab207f4f046d8
EvilDron09 <andrej32118@gmail.com>
2026-06-20 21:29:45 +0300

    Мутації та серверні дії

---
+++ src/server-action/serverActins.tsx
@@ -0,0 +1,23 @@
+'use server'
+
+import sql from 'better-sqlite3';
+import {revalidatePath} from "next/cache";
+
+const db = sql('meals.db');
+
+export const saveMeal =async (fromData:FormData) =>{
+
+    const titleValue = fromData.get('title');
+    db.prepare(`insert into meals(title) values (?)`)
+        .run(titleValue);
+    revalidatePath('/')
+}
+
+type Meal = {
+    id:number,
+    title:string
+}
+
+export const getMeals = async ():Promise<Meal[]> =>{
+    return db.prepare<Meal[]>('select * from meals').all() as Meal[];
+}

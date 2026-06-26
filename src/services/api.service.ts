1900a17088e0b83ef7d751ec6ff3d00467ec8796
EvilDron09 <andrej32118@gmail.com>
2026-06-26 18:42:39 +0300

    hw 2

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,4 +1,5 @@
 import {ICar} from "@/models/ICar";
+import {postCars} from "@/services/serviceAction";
 
 
 export const baseUrl = 'http://bigbird.space/carsAPI/v1'
@@ -10,4 +11,12 @@ export const getCars = async():Promise<ICar[]> =>{
         .then(value => value.json())
 }
 
+await fetch(`${baseUrl}/cars`, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application.json',
+    },
+    body: JSON.stringify(postCars),
+
+})
 
--- src/services/serviceAction.ts
+++ src/services/serviceAction.ts
@@ -1,19 +1,11 @@
 'use server'
 
-import {baseUrl} from "@/services/api.service";
+
 
 export const postCars = async (fromData:FormData)=>{
     const brand = fromData.get('brand') as string
     const price = fromData.get('price') as string
-    const year = fromData.get('brand') as string
+    const year = fromData.get('year') as string
 
-    await fetch(`${baseUrl}/cars`,{
-        method:'POST',
-        headers:{
-            'Content-Type':'application.json',
-        },
-        body:JSON.stringify({brand,year,price}),
+    }
 
-
-    })
-}
